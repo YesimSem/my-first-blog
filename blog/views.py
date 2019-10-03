fa3b5678e06be70b929cddcc865e9b50dabbc538
aytensemchenko <aytenyesim@protonmail.com>
2019-10-03 16:19:33 +0200

    Add a new post functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +12,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    #form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+    else:
+        form = PostForm()
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog/post_edit.html', {'form': form})
