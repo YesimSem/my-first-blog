a4940fc333011fc099afd52b17a3fff2780cbe17
aytensemchenko <aytenyesim@protonmail.com>
2019-10-03 16:40:13 +0200

    Editing an existing post

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/edit/<int:pk>', views.post_edit, name='post_edit'),
 ]
 
